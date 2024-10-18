e739004bf97e0da452db3f5a6c6e9fdaf38638e5
kaniadhea <kaniadhea7@gmail.com>
2024-10-18 17:22:38 +0700

    nyobaa

--- app/src/main/java/com/example/myapplication/class.kt
+++ app/src/main/java/com/example/myapplication/class.kt
@@ -1,9 +1,5 @@
 package com.example.myapplication
 
+class Motor()
+class Contact(val id: Int, var email: String)
 
-fun ContohList(){
-    println("==List==")
-    //List Read-Only
-    val readyOnlyAbjad = listOf("A","B","C")
-    println(readyOnlyAbjad)
-}
